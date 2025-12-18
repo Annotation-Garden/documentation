715e6cb2c730b69d6e14278a3ab2bfb9db20ad16
Seyed Yahya Shirazi <shirazi@ieee.org>
2025-12-18 14:13:53 -0800

    Configure submodules to track main branch and auto-update on build
    - Add branch = main to submodules in .gitmodules
    - Update build.sh to fetch latest from main before building
    - Update hedit submodule to v0.6.2-alpha

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ set -e
 echo "Initializing submodules..."
 git submodule update --init --recursive
 
+echo "Updating submodules to latest from main branch..."
+git submodule update --remote --merge
+
 echo "Building documentation..."
 mkdocs build
 
